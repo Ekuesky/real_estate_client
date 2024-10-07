f407372b9b80709858f49f83db50cc2491a38b0a
Ekuesky <ayiekue9127@gmail.com>
2024-10-07 16:17:31 +0000

    chore: landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
-export default function Home() {
+import type { Metadata } from "next";
+import buildings from "@/public/assets/images/buildings.webp"
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowRightIcon } from "lucide-react";
+
+
+
+export const metadata: Metadata ={
+  title: "Real estate apartments",
+  description: "Welcome to the real estate",
+  keywords: ["real estate", "apartments", "nextjs", "tailwindcss"],
+}
+
+export default function HomePage() {
   return (
-      <div >
-        <h1 className="font-robotoSlab text-6xl"> Home Page</h1>
-        <p className="text-3xl font-openSans "> This is how we do</p>
+      <div className="relative h-screen">
+        <div className="absolute inset-0 z-0">
+          <Image src={buildings} alt="Apartments" fill style={{objectFit:"cover", objectPosition:"center"}} priority />
+        </div>
+        <main className="flex-center relative z-10 h-full bg-black/50 ">
+          <div className="text-center">
+            <h1 className="font-robotoSlab mb-4 text-4xl font-semibold text-cyan-400 antialiased sm:text-6xl md:text-8xl">
+              Welcome to the real estate
+            </h1>
+            <p className="my-8 text-2xl text-teal-300 sm:text-4xl">
+              Are you a tenant or an existing tenant?
+            </p>
+            <Link href="/register" prefetch={false}>
+						<button className="bg-asparagus rounded-3xl px-4 py-2 text-lg font-semibold text-white hover:bg-lime-700 sm:px-6 sm:text-2xl">
+							<span className="inline-flex items-center">
+								Create Your Account
+									<ArrowRightIcon className="ml-2 size-6" />
+							</span>
+						</button>
+					</Link>
+          </div>
+
+        </main>
+
       </div>
   );
 }
