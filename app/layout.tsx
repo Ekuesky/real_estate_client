52da48ac88b81944d7ffd630f57564e83f6b779c
Ekuesky <ayiekue9127@gmail.com>
2024-10-07 14:56:07 +0000

    setting font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import React from "react"
+import { openSans, robotoSlab } from "@/lib/fonts";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -20,8 +12,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <html lang="en" suppressHydrationWarning>
+            <body className={`${openSans.variable} ${robotoSlab.variable} antialiased`}>
                 {children}
             </body>
         </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
       <div >
-        <h1 className="tex-6xl"> Welcome back AYIEK SKY BENJ</h1>
+        <h1 className="font-robotoSlab text-6xl"> Home Page</h1>
+        <p className="text-3xl font-openSans "> This is how we do</p>
       </div>
   );
 }
