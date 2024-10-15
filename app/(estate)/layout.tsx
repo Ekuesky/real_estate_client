707fda83a9533b3b00cefdbb2bbe2b689372a0cb
Ekuesky <ayiekue9127@gmail.com>
2024-10-15 16:06:26 +0000

    chore: left navbar for desktop device

--- app/(estate)/layout.tsx
+++ app/(estate)/layout.tsx
@@ -3,6 +3,7 @@
 // import RightNavbar from "@/components/shared/navbar/RightNavbar";
 import React from "react";
 import Navbar from "@/components/shared/navbar/Navbar";
+import LeftNavbar from "@/components/shared/navbar/LeftNavbar";
 
 interface LayoutProps {
 	children: React.ReactNode;
@@ -14,9 +15,10 @@ export default function layout({ children }: LayoutProps) {
 			<Navbar/>
 			<div className="flex">
 				{/* Left Navbar*/}
-				<div className="dark:text--pumpkin hidden text-xl md:block ">
-					Left Navbar
-				</div>
+				{/*<div className="dark:text--pumpkin hidden text-xl md:block ">*/}
+				{/*	Left Navbar*/}
+				{/*</div>*/}
+				<LeftNavbar/>
 
 				<section className="flex min-h-screen flex-1 flex-col px-4 pb-6 pt-24 sm:px-6 lg:px-8 lg:pt-32">
 					<div>
--- components/shared/navbar/MobileNavbar.tsx
+++ components/shared/navbar/MobileNavbar.tsx
@@ -1,11 +1,13 @@
-"use client";
+"use client"; // Indicates this is a Client Component in Next.js
+
+// Import necessary dependencies and components
 import { Button } from "@/components/ui/button";
 import {
-	Sheet,
-	SheetClose,
-	SheetContent,
-	SheetFooter,
-	SheetTrigger,
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
 } from "@/components/ui/sheet";
 import { leftNavLinks } from "@/constants";
 import { HomeModernIcon } from "@heroicons/react/24/solid";
@@ -13,79 +15,98 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+// Component for the content of the left navigation
 function LeftNavContent() {
-	const pathname = usePathname();
-	return (
-		<section className="flex h-full flex-col gap-6 pt-16">
-			{leftNavLinks.map((linkItem) => {
-				const isActive =
-					(pathname.includes(linkItem.path) && linkItem.path.length > 1) ||
-					pathname === linkItem.path;
-				return (
-					<SheetClose asChild key={linkItem.path}>
-						<Link
-							href={linkItem.path}
-							className={`${isActive ? "electricIndigo-gradient text-babyPowder rounded-lg" : "text-baby_richBlack"} flex items-center justify-start gap-4 bg-transparent p-4`}
-						>
-							<Image
-								src={linkItem.imgLocation}
-								alt={linkItem.label}
-								width={22}
-								height={22}
-								className={`${isActive ? "" : "color-invert"}`}
-							/>
-							<p className={`${isActive ? "base-bold" : "base-medium"}`}>
-								{linkItem.label}
-							</p>
-						</Link>
-					</SheetClose>
-				);
-			})}
-		</section>
-	);
+  // Get the current pathname for active link highlighting
+  const pathname = usePathname();
+
+  return (
+    <section className="flex h-full flex-col gap-6 pt-16">
+      {/* Map through the navigation links */}
+      {leftNavLinks.map((linkItem) => {
+        // Determine if the current link is active
+        const isActive =
+          (pathname.includes(linkItem.path) && linkItem.path.length > 1) ||
+          pathname === linkItem.path;
+
+        return (
+          // Wrap each link in SheetClose for automatic closing on click
+          <SheetClose asChild key={linkItem.path}>
+            <Link
+              href={linkItem.path}
+              className={`${
+                isActive
+                  ? "electricIndigo-gradient text-babyPowder rounded-lg"
+                  : "text-baby_richBlack"
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              {/* Icon for the navigation link */}
+              <Image
+                src={linkItem.imgLocation}
+                alt={linkItem.label}
+                width={22}
+                height={22}
+                className={`${isActive ? "" : "color-invert"}`}
+              />
+              {/* Label for the navigation link */}
+              <p className={`${isActive ? "base-bold" : "base-medium"}`}>
+                {linkItem.label}
+              </p>
+            </Link>
+          </SheetClose>
+        );
+      })}
+    </section>
+  );
 }
 
+// Main component for the mobile navbar
 export default function MobileNavbar() {
-	return (
-		<Sheet>
-			<SheetTrigger asChild className="cursor-pointer">
-				<Image
-					src="/assets/icons/mobile-menu.svg"
-					alt="Mobile Menu"
-					width={36}
-					height={36}
-					className="invert-colors sm:hidden"
-				/>
-			</SheetTrigger>
-			<SheetContent side="left" className="bg-baby_rich border-none">
-				<Link href="/" className="flex items-center gap-1">
-					<HomeModernIcon className="mr-2 size-11 text-lime-500" />
-					<p className="h2-bold text-baby_veryBlack font-robotoSlab">
-						Ayiek <span className="text-lime-500"> Apartments</span>
-					</p>
-				</Link>
+  return (
+    <Sheet>
+      {/* Trigger for opening the mobile menu */}
+      <SheetTrigger asChild className="cursor-pointer">
+        <Image
+          src="/assets/icons/mobile-menu.svg"
+          alt="Mobile Menu"
+          width={36}
+          height={36}
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      {/* Content of the mobile menu */}
+      <SheetContent side="left" className="bg-baby_rich border-none">
+        {/* Logo and site name */}
+        <Link href="/" className="flex items-center gap-1">
+          <HomeModernIcon className="mr-2 size-11 text-lime-500" />
+          <p className="h2-bold text-baby_veryBlack font-robotoSlab">
+            Ayiek <span className="text-lime-500"> Apartments</span>
+          </p>
+        </Link>
 
-				<div>
-					<SheetClose asChild>
-						<LeftNavContent />
-					</SheetClose>
+        <div>
+          {/* Navigation links */}
+          <SheetClose asChild>
+            <LeftNavContent />
+          </SheetClose>
 
-					<SheetClose asChild>
-						<SheetFooter>
-									<Link href="/register">
-										<Button className="electricIndigo-gradient small-medium light-border-2 btn-tertiary text-babyPowder mt-4 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-											Register
-										</Button>
-									</Link>
-									<Link href="/login">
-										<Button className="lime-gradient small-medium light-border-2 btn-tertiary text-babyPowder min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
-											Login
-										</Button>
-									</Link>
-						</SheetFooter>
-					</SheetClose>
-				</div>
-			</SheetContent>
-		</Sheet>
-	);
+          {/* Authentication buttons */}
+          <SheetClose asChild>
+            <SheetFooter>
+              <Link href="/register">
+                <Button className="electricIndigo-gradient small-medium light-border-2 btn-tertiary text-babyPowder mt-4 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                  Register
+                </Button>
+              </Link>
+              <Link href="/login">
+                <Button className="lime-gradient small-medium light-border-2 btn-tertiary text-babyPowder min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                  Login
+                </Button>
+              </Link>
+            </SheetFooter>
+          </SheetClose>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
