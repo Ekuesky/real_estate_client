b9d8ce6103bae545c2c4433407ea044505d4ff6b
Ekuesky <ayiekue9127@gmail.com>
2024-11-06 19:01:38 +0000

    added social auth hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,1 +1,3 @@
 export {useAuthNavigation} from "@/hooks/useAuthNavigation"
+export { default as useSocialAuth} from "@/hooks/useSocialAuth"
+export {default as useRedirectIfAuthenticated} from "@/hooks/useRedirectIfAuthenticated"
---
+++ hooks/useSocialAuth.ts
@@ -0,0 +1,33 @@
+import { useAppDispatch } from "@/lib/redux/hooks/typedHooks";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { setAuth } from "@/lib/redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+
+
+export default function useSocialAuth(authenticate:any, provider:string){
+	const dispatch = useAppDispatch()
+	const router = useRouter()
+	const searchParams = useSearchParams()
+
+	const effectRan = useRef(false)
+
+	useEffect(() =>{
+		const state = searchParams.get('state')
+		const code = searchParams.get('code')
+
+		if (state && code && !effectRan.current){
+			authenticate({provider, state, code}).unwrap().then(() =>{
+				dispatch(setAuth())
+				toast.success("Logged in successfully")
+				router.push("/welcome")
+			}).catch(() =>{
+				toast.error("An error occurred while trying to authenticate")
+        router.push("/auth/login")
+			})
+		}
+		return ()=>{
+			effectRan.current = true;
+		};
+	}, [authenticate, dispatch, provider, router, searchParams]);
+}
